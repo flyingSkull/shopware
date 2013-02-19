875268dedd6c227bd9a2cb31ac5dc6ca43fd5685
Ricky Flugrat <ricky.flugrat@googlemail.com>
2013-02-19 21:12:50 +0100

    config.php aktualisiert

--- config.php
+++ config.php
@@ -1,18 +1,19 @@
 <?php
 return array(
     'db' => array(
-    /*
+    
         'username' => 'p159946',
         'password' => 'H7jEnVUP',
         'dbname' => 'usr_p159946_1',
         'host' => 'db1095.mydbserver.com',
         'port' => '3306'
-     */  
-        
+
+      /*
         'username' => 'root',
     	'password' => 'root',
     	'host' => 'localhost',
     	'dbname' => 'usr_p159946_1',
     	'port' => '3306'
+      */
     )
 );
