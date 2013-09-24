89cbe88b1657dc3799b7b3015368eab4821fad9a
p159946 <p159946@localdomain>
2013-09-25 00:03:27 +0200

    plugins added

---
+++ engine/Shopware/Plugins/Community/Frontend/SwagCaptcha/Meta.php
@@ -0,0 +1,32 @@
+<?php
+return array(
+	'version' => '1.0.5',
+	'autor' => 'shopware AG, d.giesler',
+	'copyright' => 'Copyright © 2012, shopware AG',
+	'label' => 'SwagCaptcha',
+	'name' => 'SwagCaptcha',
+	'description' => 'This plugin adds a reCaptcha field on the register site.',
+	'support' => 'http://forum.shopware.de',
+	'link' => 'http://www.shopware.de',
+	'changes' => array(
+		'1.0.0'=>array('releasedate'=>'2011-09-16', 'lines' => array(
+			'First release'
+		)),
+		'1.0.1'=>array('releasedate'=>'2011-09-21', 'lines' => array(
+			'Fixed a bug, which disabled the password-change function'
+		)),
+		'1.0.2'=>array('releasedate'=>'2011-12-01', 'lines' => array(
+			'Refactored some code'
+		)),
+		'1.0.3'=>array('releasedate'=>'2011-12-02', 'lines' => array(
+			'Added multilingual support'
+		)),
+		'1.0.4'=>array('releasedate'=>'2011-12-02', 'lines' => array(
+			'Removed combo-box for Shopware 3.5.3 support'
+		)),
+		'1.0.5'=>array('releasedate'=>'2012-10-18', 'lines' => array(
+			'Updated for Shopware 4.0'
+		))
+	),
+	'revision' => '5'
+);
