b9c5fefb88ccf73cd3d3feb00359f21ad948c01e
Ricky Flugrat <ricky.flugrat@googlemail.com>
2013-11-14 22:34:36 +0100

    clear git- cache

--- update_4.1.3/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-define('SW_PATH', realpath(__DIR__ . '/../'));
-define('UPDATE_PATH', __DIR__);
-
-$app = require __DIR__ . '/src/app.php';
-
-$app->run();
--- update_4.1.3/src/templates/noaccess.php
+++
@@ -1,9 +0,0 @@
-<!-- Start page -->
-<div id="start">
-    <div class="page-header">
-        <h2><?php echo $language["noaccess_title"];?></h2>
-    </div>
-    <div class="alert alert-error">
-        <?php printf($language["noaccess_info"], $clientIp, $filePath); ?>
-    </div>
-</div>
